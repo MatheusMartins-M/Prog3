56ab6983f1735e281ffea119ac6b1dd2b8f06404
MatheusMartins-M <127354359+MatheusMartins-M@users.noreply.github.com>
2024-09-16 22:05:04 -0300

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,32 @@
+let a, x, y, texto, w;
+let m = "10";
+
+const b = "abacate";
+a = "casa";
+a = "melancia"
+
+x = 10;
+y = 3;
+w = x + y;
+
+texto = "A soma de "+ x + " com "+ y +" é igual a "+ w;
+
+console.log(a);
+console.log(b);
+console.log("-------------------------------");
+console.log(texto);
+console.log("-------------------------------");
+
+if(x % y == 0){
+    console.log("Ok, está certo!");
+}else{
+    console.log("Tem algo errado, revise");
+}
+
+console.log("-------------------------------");
+
+for(let i = 1; i <= 5; i++){
+    console.log("Valor: "+ i)
+}
+
+console.log("-------------------------------");
