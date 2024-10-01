932f1d65d579b8361a4efeeb090211b142963cb7
MatheusMartins-M <127354359+MatheusMartins-M@users.noreply.github.com>
2024-09-30 21:54:42 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,38 +1,13 @@
-console.log("OK!")
+console.log("Aula part 2")
+console.log("")
 
-let exemplo = "texto";
-console.log(exemplo.length);
-console.log(exemplo.toUpperCase());
-console.log(exemplo.indexOf("xt"));
-console.log(exemplo[2])
-console.log(exemplo[0])
-console.log(exemplo[exemplo.length - 1]);
-console.log(exemplo.toUpperCase().indexOf("TO"));
+import readline from "readline";
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-let email2 = "www.restiga.ifrs.edu.br";
-let email = "matheus.m.mota50@restinga.ifrs.edu.br";
-
-if(email2.indexOf("@") >= 1 && email2.endsWith("restinga.ifrs.edu.br")){
-    console.log("Aceito!");
-}else{
-    console.log("Negado!")
-}
-
-if(email.indexOf("@") >= 1 && email.endsWith("restinga.ifrs.edu.br")){
-    console.log("Aceito!");
-}else{
-    console.log("Negado!")
-}
-
-let lista = ["a", "b", "c"];
-
-for(const chave in lista){
-    console.log(chave);
-    console.log(lista[chave])
-    console.log("========");
-}
-
-for(const valor of lista){
-    console.log(valor)
-}
+rl.question("Qual seu nome? ", (name) => {
+    console.log("Olá, "+ name);
+});
 
